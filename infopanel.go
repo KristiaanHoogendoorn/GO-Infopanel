9fcac3f56b6ff0cbdb2dc10a82be8c7998156741
Kristiaan Hoogendoorn <khoogendoorn@bol.com>
2020-01-02 16:13:13 +0100

    Get to stuc done, now documentation and....... GUI

--- infopanel.go
+++ infopanel.go
@@ -1,38 +1,63 @@
 package main
 
 import (
-	//"bytes"
+	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"net/http"
+	"time"
 )
+
+type weather struct {
+	Liveweer []struct {
+		Plaats     string
+		Temp       string
+		Samenv     string
+		Lv         string
+		Windr      string
+		Windms     string
+		Winds      string
+		Windk      string
+		Windkmh    string
+		Luchtd     string
+		Ldmmhg     string
+		Dauwp      string
+		Zicht      string
+		Verw       string
+		Sup        string
+		Sunder     string
+		Image      string
+		D0Weer     string
+		D0Tmax     string
+		D0Tmin     string
+		D0Windk    string
+		Gtemp      string
+		D0Windknp  string
+		D0Windms   string
+		D0Windkmh  string
+		D0Windr    string
+		D0Neerslag string
+		Alarm      string
+	}
+}
+
+var myClient = &http.Client{Timeout: 10 * time.Second}
 
 const weeronline_API_key string = "2aa4dd2560"
 
-func Get_wether(Location string) string {
+func GetWeather(Location string, target interface{}) error {
 	fmt.Println("Starting the application...")
-	response, err := http.Get(fmt.Sprintf("http://weerlive.nl/api/json-data-10min.php?key=%s&locatie=%s", weeronline_API_key, Location))
+	response, err := myClient.Get(fmt.Sprintf("http://weerlive.nl/api/json-data-10min.php?key=%s&locatie=%s", weeronline_API_key, Location))
 	if err != nil {
 		fmt.Printf("Applicatoin failed to connect to API: %s\n", err)
-	} else {
-		//data := { "liveweer": [{"plaats": "Utrecht", "temp": "9.2", "gtemp": "6.2", "samenv": "Af en toe lichte regen", "lv": "90", "windr": "West", "windms": "6", "winds": "4", "windk": "11.7", "windkmh": "21.6", "luchtd": "1005.5", "ldmmhg": "754", "dauwp": "7", "zicht": "4", "verw": "Geleidelijk enkele buien. Eerste kerstdag nog een enkele bui", "sup": "08:44", "sunder": "16:33", "image": "buien", "d0weer": "halfbewolkt", "d0tmax": "10", "d0tmin": "6", "d0windk": "3", "d0windknp": "10", "d0windms": "5", "d0windkmh": "19", "d0windr": "NO", "d0neerslag": "33", "d0zon": "28", "d1weer": "halfbewolkt", "d1tmax": "9", "d1tmin": "6", "d1windk": "3", "d1windknp": "8", "d1windms": "4", "d1windkmh": "15", "d1windr": "W", "d1neerslag": "50", "d1zon": "30", "d2weer": "halfbewolkt", "d2tmax": "5", "d2tmin": "2", "d2windk": "2", "d2windknp": "6", "d2windms": "3", "d2windkmh": "11", "d2windr": "Z", "d2neerslag": "40", "d2zon": "20", "alarm": "0"}]}
-
-		data, _ := ioutil.ReadAll(response.Body)
-		return string(data)
 	}
-	return ""
+	defer response.Body.Close()
+	return json.NewDecoder(response.Body).Decode(target)
 }
 
-//func ()  {
-//
-//}
-
 func main() {
-	fmt.Println(Get_wether("Utrecht"))
-	//for i := range Get_wether("Utrecht"){
-	//	fmt.Println(Get_wether("Utrecht"))
-	//	fmt.Println(i)
-	//}
+	data1 := new(weather)
+	GetWeather("Utrecht", data1)
+	fmt.Println(data1.Liveweer[0].Plaats)
 }
 
 //#post data example
